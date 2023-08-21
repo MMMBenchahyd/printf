8e02d4d596e190a1443d900ee7cd52bea52c00fb
zakariiia <zakariamoukrim57@gmail.com>
2023-08-21 10:41:31 -0700

    _printf.c betty style edite

--- _printf.c
+++ _printf.c
@@ -7,38 +7,32 @@
  */
 
 
-    int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-    int printedchar = 0;
-    char *charr;
-    va_list ap;
+        int printedchar = 0;
+        char *charr;
+        va_list ap;
+        va_start(ap, format);
+        for (int i=0; *format && format[i] != '\0'; i++)
+            {
+                if (format[i] == '%')
+                    {
+                        i++;
+                        if (format[i] == 'c')
+                            {
+                                charr = va_arg(ap, char *);
+                                for(int j=0; charr[j] != '\0'; j++)
+                                    {
+                                        write(1, &charr[j], 1);
+                                        printedchar++;
+                                    }
+                                i++;
+                            }  
+                    }
+                write(1, &format[i], 1);
+                printedchar++;
+                va_end(ap);
 
-    va_start(ap, format);
-
-
-    for (int i=0; *format && format[i] != '\0'; i++)
-        {
-
-        if (format[i] == '%')
-
-        {
-            i++;
-            if (format[i] == 'c')
-                {
-                charr = va_arg(ap, char *);
-
-                for(int j=0; charr[j] != '\0'; j++){
-
-                    write(1, &charr[j], 1);
-                    printedchar++;
-                }
-            i++;
             }
-        }
-        write(1, &format[i], 1);
-        printedchar++;
-        va_end(ap);
-
+        return (printedchar);
     }
-    return (printedchar);
-}
