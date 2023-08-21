64048fc2cf077933dc5499d9d2fb9a9c88fc5f7a
zakariiia <zakariamoukrim57@gmail.com>
2023-08-21 12:21:00 -0700

    fixing betty style check

--- _printf.c
+++ _printf.c
@@ -6,32 +6,26 @@
  * Return: Printed chars.
  */
 
-
-int _printf(const char *format, ...)
-{
-    int printedchar = 0;
-    char *charr;
-    va_list ap;
-    va_start(ap, format);
-    for (int i = 0; *format && format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-                if (format[i] == 'c')
-                {
-                    charr = va_arg(ap, char *);
-                        for (int j = 0; charr[j] != '\0'; j++)
-                        {
-                            write(1, &charr[j], 1);
-                            printedchar++;
+int _printf(const char * format, ...) {
+        int printedchar = 0;
+        char * charr;
+        va_list ap;
+        va_start(ap, format);
+        for (int i = 0;* format && format[i] != '\0'; i++) {
+                if (format[i] == '%') {
+                        i++;
+                        if (format[i] == 'c') {
+                                charr = va_arg(ap, char * );
+                                for (int j = 0; charr[j] != '\0'; j++) {
+                                        write(1, & charr[j], 1);
+                                        printedchar++;
+                                }
+                                i++;
                         }
-                    i++;
-                }  
+                }
+                write(1, & format[i], 1);
+                printedchar++;
+                va_end(ap);
         }
-        write(1, &format[i], 1);
-        printedchar++;
-        va_end(ap);
-    }
-    return (printedchar);
+        return (printedchar);
 }
