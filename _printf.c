eee4c6cb97925da585478af80a7f6b3a56f5302a
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2023-08-21 12:02:39 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -9,29 +9,35 @@
 
 int _printf(const char *format, ...)
 {
-    int printedchar = 0;
-    char *charr;
-    va_list ap;
-    va_start(ap, format);
-    for (int i = 0; *format && format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                charr = va_arg(ap, char *);
-                for(int j = 0; charr[j] != '\0'; j++)
-                {
-                    write(1, &charr[j], 1);
-                    printedchar++;
-                }
-                i++;
-            }  
-        }
-        write(1, &format[i], 1);
-        printedchar++;
-        va_end(ap);
-    }
-    return (printedchar);
+	int printedchar = 0;
+	char *charr;
+	va_list ap;
+
+	va_start(ap, format);
+	for (int i = 0; *format && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				charr = va_arg(ap, char *);
+				printedchar++;
+			}
+			if (format[i] == 's')
+			{
+				charr = va_arg(ap, char *);
+				for (int j = 0; charr[j] != '\0'; j)
+				{
+					write(1, &charr[j], 1);
+					printedchar++;
+				}
+				i++;
+			}
+		}
+		write(1, &format[i], 1);
+		printedchar++;
+		va_end(ap);
+	}
+	return (printedchar);
 }
