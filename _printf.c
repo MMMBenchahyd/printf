06ec8d10d5914f8724c4e4fb5ae24dbb2e4a1bd9
zakariiia <zakariamoukrim57@gmail.com>
2023-08-21 11:34:37 -0700

    betty style fix

--- _printf.c
+++ _printf.c
@@ -9,30 +9,30 @@
 
 int _printf(const char *format, ...)
 {
-        int printedchar = 0;
-        char *charr;
-        va_list ap;
-        va_start(ap, format);
-        for (int i=0; *format && format[i] != '\0'; i++)
+    int printedchar = 0;
+    
+    char *charr;
+    va_list ap;
+    va_start(ap, format);
+    for (int i=0; *format && format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
             {
-                if (format[i] == '%')
-                    {
-                        i++;
-                        if (format[i] == 'c')
-                            {
-                                charr = va_arg(ap, char *);
-                                for(int j=0; charr[j] != '\0'; j++)
-                                    {
-                                        write(1, &charr[j], 1);
-                                        printedchar++;
-                                    }
-                                i++;
-                            }  
-                    }
-                write(1, &format[i], 1);
-                printedchar++;
-                va_end(ap);
-
-            }
-        return (printedchar);
+                charr = va_arg(ap, char *);
+                for(int j=0; charr[j] != '\0'; j++)
+                {
+                    write(1, &charr[j], 1);
+                    printedchar++;
+                }
+                i++;
+            }  
+        }
+        write(1, &format[i], 1);
+        printedchar++;
+        va_end(ap);
     }
+    return (printedchar);
+}
